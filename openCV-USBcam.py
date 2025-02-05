21594fb26e5d1e91267576ac0c5508d473c626e9
TDK74 <98406593+TDK74@users.noreply.github.com>
2025-02-05 22:37:10 +0200

    Added a Python file with openCV
    Used USB camera instead of CSI camera as in the lesson but used openCV for proper work of the camera.
    Colours are not correct.

---
+++ openCV-USBcam.py
@@ -0,0 +1,34 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+if not cap.isOpened():
+    print("Can't open the USB camera.")
+    exit()
+
+try:
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Can't read the frame.")
+            break
+
+        cv2.imshow("UsbCam", frame)
+        #cv2.moveWindow("UsbCam", 100, 100)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        if cv2.waitKey(1) == 27:  # '27' is ASCII code for 'ESC'
+            break
+
+except KeyboardInterrupt:
+    print("User's Ctrl+C detected.")
+
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
