c1e193dcfa6878b9fe4218ced747f292f2e95bc0
TDK74 <98406593+TDK74@users.noreply.github.com>
2025-02-05 22:27:18 +0200

    Added a Python file from Lesson 45.
    Used USB camera instead of CSI camera as in the lesson but still worked.
    Problem: colours are not correct.

---
+++ openCV-1.py
@@ -0,0 +1,28 @@
+import cv2
+from picamera2 import Picamera2 as Pcm2
+
+
+piCam = Pcm2()
+piCam.preview_configuration.main.size = (1280, 720)
+piCam.preview_configuration.main.format = "RGB888"
+piCam.preview_configuration.align()
+piCam.configure("preview")
+piCam.start()
+
+try:
+    while True:
+        frame = piCam.capture_array()
+        cv2.imshow("piCam", frame)
+        #cv2.moveWindow("piCam", 100, 100)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+        if cv2.waitKey(1) == 27: # '27' is ASCII code for 'ESC' # Press and hold.
+            break
+        
+except KeyboardInterrupt:
+    print("User's Ctrl+C detected.")
+
+finally:
+    cv2.destroyAllWindows()
