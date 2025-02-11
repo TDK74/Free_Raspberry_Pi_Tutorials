378e749061723611b7cbde067098e026a767cfba
TDK74 <98406593+TDK74@users.noreply.github.com>
2025-02-11 15:58:05 +0200

    Added a Python file with openCV for USB camera.

---
+++ face-eyes-detect_USBcam.py
@@ -0,0 +1,73 @@
+import cv2
+from time import time
+
+
+cap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier("./haar/haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier("./haar/haarcascade_eye_tree_eyeglasses.xml")
+
+dispW = int(1280 / 2)
+dispH = int(720 / 1.5)
+fps = 0.0
+pos = (30, 60)
+font = cv2.FONT_HERSHEY_SIMPLEX
+myColour = (255, 0, 0)
+height = 1.5
+weight = 3
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, dispW)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, dispH)
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+if not cap.isOpened():
+    print("Can't open the USB camera.")
+    exit()
+
+try:
+    while True:
+        tStart = time()
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Can't read the frame.")
+            break
+
+        #frame = cv2.flip(frame, -1)    # if the frame needs flipping
+        frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(frameGray, 1.3, 5)
+
+        for face in faces:
+                x, y, w, h = face
+                cv2.rectangle(frame, (x, y), (x + w, y + h),
+                                (255, 0, 0), 2)
+                roiGray = frameGray[y : y + h, x : x + w]
+                roiColour = frame[y : y + h, x : x + w]
+                eyes = eyeCascade.detectMultiScale(roiGray)
+
+                for eye in eyes:
+                        x, y, w, h, = eye
+                        cv2.rectangle(roiColour, (x, y), (x + w, y + h),
+                                        (255, 0, 0), 2)
+
+        cv2.putText(frame, 'FPS: ' + str(int(fps)),
+                    pos, font, height, myColour, weight)
+        cv2.imshow("UsbCam", frame)
+        #cv2.moveWindow("UsbCam", 100, 100)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        if cv2.waitKey(1) == 27:  # '27' is ASCII code for 'ESC' # Press and hold.
+            break
+
+        tEnd = time()
+        loopTime = tEnd - tStart
+        fps = (0.9 * fps) + (0.1 * 1 / loopTime) # low pass filter
+        #print("FPS is: ", int(fps))
+
+except KeyboardInterrupt:
+    print("User's Ctrl+C detected.")
+
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
